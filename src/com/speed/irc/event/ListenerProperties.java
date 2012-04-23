8e2485bad6c9a970c9a6f99436873d7898e96358
Shivam Mistry <shivam9000@gmail.com>
2012-04-23 21:20:30 +0100

    Added LGPL docs to ListenerProperties

--- src/com/speed/irc/event/ListenerProperties.java
+++ src/com/speed/irc/event/ListenerProperties.java
@@ -3,6 +3,28 @@ package com.speed.irc.event;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 
+/**
+ * Event listeners must use this annotation to allow them to receive the correct
+ * events.
+ * 
+ * This file is part of Speed's IRC API.
+ * 
+ * Speed's IRC API is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as published by the
+ * Free Software Foundation, either version 3 of the License, or (at your
+ * option) any later version.
+ * 
+ * Speed's IRC API is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License
+ * for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with Speed's IRC API. If not, see <http://www.gnu.org/licenses/>.
+ * 
+ * @author Shivam Mistry
+ * 
+ */
 @Retention(value = RetentionPolicy.RUNTIME)
 public @interface ListenerProperties {
 	Class<? extends IRCEvent>[] events();
